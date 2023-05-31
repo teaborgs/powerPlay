29d88f08eafc20cce72fc0b1a598b2c7e1000675
Vlad_ <vladup07@gmail.com>
2023-05-31 18:16:59 +0300

    Create SingleMotorTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Testing/SingleMotorTest.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.drive.Testing;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+
+
+@TeleOp(group = "Testing")
+public class SingleMotorTest extends LinearOpMode
+{
+
+    DcMotorEx motor;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        Init();
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested())
+            RunLoop();
+    }
+
+    private void Init()
+    {
+        motor = hardwareMap.get(DcMotorEx.class, "leftFront"); // slot 0
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+    }
+
+    private void RunLoop()
+    {
+        motor.setPower(gamepad1.right_stick_y);
+    }
+
+
+
+    private double DriveFn(double x)
+    {
+        double sign = x / Math.abs(x);
+        return (x * x * sign);
+    }
+}
