263d2d55cdec4492ec09c4fc1ed04b08eeda4019
Vlad_ <vladup07@gmail.com>
2023-06-22 20:25:00 +0300

    Industrial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Misc/CustomPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Misc/CustomPipeline.java
@@ -6,35 +6,60 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+import java.math.BigInteger;
+
 public class CustomPipeline extends OpenCvPipeline
 {
     enum Detection
     {
-        //
+        None,
+        Red,
+        Blue,
+        Unknown
     }
 
-    Detection lastDetection;
+    Detection lastDetection = Detection.None;
+
+    private final Object decimationSync = new Object();
 
     @Override
     public Mat processFrame(Mat input)
     {
+        long rAvg = 0, gAvg = 0, bAvg = 0;
 
-        // TODO: detectie
 
-        for (int i = 0; i < input.cols(); i++)
-            for (int j = 0; j < input.rows(); j++)
-                x = input.get(i, j);
+        for (int i = 0; i < input.height(); i++)
+        {
+            for (int j = 0; j < input.width(); j++)
+            {
+                double[] pixel = input.get(i, j);
+
+                rAvg += pixel[0];
+                gAvg += pixel[1];
+                bAvg += pixel[2];
+            }
+        }
+
+        rAvg /= ((long) input.width() * input.height());
+        gAvg /= ((long) input.width() * input.height());
+        bAvg /= ((long) input.width() * input.height());
+
+        if (rAvg > 170 && rAvg > bAvg)
+            lastDetection = Detection.Red;
+        else if (bAvg > 170 && bAvg > rAvg)
+            lastDetection = Detection.Blue;
 
 
         Imgproc.rectangle(
                 input,
                 new Point(
-                        input.cols()/4,
-                        input.rows()/4),
+                        input.cols()/4f,
+                        input.rows()/4f),
                 new Point(
                         input.cols()*(3f/4f),
                         input.rows()*(3f/4f)),
-                new Scalar(0, 255, 0), 4);
+                new Scalar((lastDetection == Detection.Red ? 255 : 0), 0, (lastDetection == Detection.Blue ? 255 : 0)), 4);
+
 
         return input;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Misc/IndustrialDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Misc/IndustrialDemo.java
@@ -2,14 +2,23 @@ package org.firstinspires.ftc.teamcode.drive.Misc;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.autonom.OpenCV.AprilTagDetectionPipeline;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 
 @Autonomous(group = "autonom")
 public class IndustrialDemo extends LinearOpMode
 {
     SampleMecanumDrive mecanumDrive;
+    OpenCvCamera camera;
+    CustomPipeline pipeline;
+    int cameraMonitorViewId = 0;
+
     @Override
     public void runOpMode() throws InterruptedException
     {
@@ -23,6 +32,27 @@ public class IndustrialDemo extends LinearOpMode
 
     private void Init()
     {
+        cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        pipeline = new CustomPipeline();
+
+        camera.setPipeline(pipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+
         telemetry.setMsTransmissionInterval(100);
     }
 
@@ -33,10 +63,7 @@ public class IndustrialDemo extends LinearOpMode
 
     private void Telemetry()
     {
-        telemetry.addLine("Reading");
-
-
-
+        telemetry.addLine(pipeline.GetLastDetection().name());
         telemetry.update();
     }
 }
