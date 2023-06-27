5889df899afa10879bc0810c58c4c9dcc26689fb
Vlad-07 <vladup07@gmail.com>
2023-06-27 18:07:40 +0300

    IndustrialDemo detectie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Misc/CustomPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Misc/CustomPipeline.java
@@ -6,66 +6,212 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Objects;
 
-public class CustomPipeline extends OpenCvPipeline
+public final class CustomPipeline extends OpenCvPipeline
 {
-    enum Detection
-    {
-        None,
-        Red,
-        Blue,
-        Unknown
-    }
+	private static class Pixel
+	{
+		Pixel()
+		{
+			r = 0;
+			g = 0;
+			b = 0;
+			a = 0;
+		}
+		Pixel(int _r, int _g, int _b, int _a)
+		{
+			r = _r;
+			g = _g;
+			b = _b;
+			a = _a;
+		}
+		Pixel(double[] data)
+		{
+			r = data[0];
+			g = data[1];
+			b = data[2];
+			a = data[3];
+		}
 
-    Detection lastDetection = Detection.None;
+		public final double[] GetData()
+		{
+			return new double[]{ r, g, b, a };
+		}
 
-    private final Object decimationSync = new Object();
+		public double r, g, b, a;
+	}
 
-    @Override
-    public Mat processFrame(Mat input)
-    {
-        long rAvg = 0, gAvg = 0, bAvg = 0;
+	public final static class Detection
+	{
+		public enum Color
+		{
+			None, Red, Blue, Unknown
+		}
+
+		public enum Shape
+		{
+			None, Cube, Sphere, Unknown
+		}
+
+		Detection()
+		{
+			Reset();
+		}
+
+		Detection(Color _color, Shape _shape)
+		{
+			color = _color;
+			shape = _shape;
+		}
+
+		public void Reset()
+		{
+			color = Color.None;
+			shape = Shape.None;
+		}
+
+		Color color;
+		Shape shape;
+	}
+
+	private Detection _lastDetection = new Detection();
+	private boolean _debugMode = true;
+	private boolean _pauseDetection = false;
+	private int _approximation = 4;
+
+	@Override
+	public Mat processFrame(Mat image)
+	{
+		if (_pauseDetection)
+			return image;
+
+		Detection detection = new Detection();
+
+		Mat processed = new Mat(image.rows() / _approximation, image.cols() / _approximation, image.type());
+
+		long redArea = 0, blueArea = 0;
+		for (int i = 0; i < image.height(); i += _approximation)
+		{
+			for (int j = 0; j < image.width(); j += _approximation)
+			{
+				Pixel pixel = new Pixel(image.get(i, j));
+
+				if (pixel.g > 150 || pixel.g > pixel.r && pixel.g > pixel.b) // ignore too much green or green dominant
+				{
+					processed.put(i / _approximation, j / _approximation, 0, 0, 0, 255);
+					continue;
+				}
+
+				if (IsGray(pixel)) // ignore shades of gray
+				{
+					processed.put(i / _approximation, j / _approximation, 0, 0, 0, 255);
+					//CullPixels(image, i, j, _approximation, 0, 0, 0, 255);
+					continue;
+				}
+
+				if (pixel.r > pixel.g && pixel.r > pixel.b) // red is dominant
+				{
+					processed.put(i / _approximation, j / _approximation, 255, 0, 0, 255);
+					//CullPixels(image, i, j, _approximation, 255, 0, 0, 255);
+					redArea++;
+				}
+				else if (pixel.b > pixel.r && pixel.b > pixel.g) // blue is dominant
+				{
+					processed.put(i / _approximation, j / _approximation, 0, 0, 255, 255);
+					//CullPixels(image, i, j, _approximation, 0, 0, 255, 255);
+					blueArea++;
+				}
+			}
+		}
+
+		if (redArea > blueArea)
+			detection.color = Detection.Color.Red;
+		else
+			detection.color = Detection.Color.Blue;
 
 
-        for (int i = 0; i < input.height(); i++)
-        {
-            for (int j = 0; j < input.width(); j++)
-            {
-                double[] pixel = input.get(i, j);
+		double[] bounds = FindBounds(processed);
+		double boundingBoxArea = (bounds[1] - bounds[0]) * (bounds[3] - bounds[2]);
+		double areaRatio = (detection.color == Detection.Color.Red ? redArea : blueArea) / boundingBoxArea;
+		if (areaRatio < 0.85) // a bit more than pi/4
+			detection.shape = Detection.Shape.Sphere;
+		else
+			detection.shape = Detection.Shape.Cube;
 
-                rAvg += pixel[0];
-                gAvg += pixel[1];
-                bAvg += pixel[2];
-            }
-        }
+		if (_debugMode)
+			Imgproc.rectangle(processed, new Point(bounds[0], bounds[2]), new Point(bounds[1], bounds[3]), new Scalar((detection.shape == Detection.Shape.Cube ? 255 : 0), 255, (detection.shape == Detection.Shape.Sphere ? 255 : 0), 255), 1);
 
-        rAvg /= ((long) input.width() * input.height());
-        gAvg /= ((long) input.width() * input.height());
-        bAvg /= ((long) input.width() * input.height());
+		_lastDetection = detection;
 
-        if (rAvg > 170 && rAvg > bAvg)
-            lastDetection = Detection.Red;
-        else if (bAvg > 170 && bAvg > rAvg)
-            lastDetection = Detection.Blue;
+		return processed;
+	}
 
+	public Detection GetLastDetection() { return _lastDetection; }
 
-        Imgproc.rectangle(
-                input,
-                new Point(
-                        input.cols()/4f,
-                        input.rows()/4f),
-                new Point(
-                        input.cols()*(3f/4f),
-                        input.rows()*(3f/4f)),
-                new Scalar((lastDetection == Detection.Red ? 255 : 0), 0, (lastDetection == Detection.Blue ? 255 : 0)), 4);
+	public int GetApproximation() { return _approximation; }
+	public void SetApproximation(int approx) { _approximation = approx; }
 
+	public boolean GetDebug() { return _debugMode; }
+	public void SetDebug(boolean debug) { _debugMode = debug; }
 
-        return input;
-    }
+	public void Pause() { _pauseDetection = true; }
+	public void Unpause() { _pauseDetection = false; }
 
-    public Detection GetLastDetection()
-    {
-        return lastDetection;
-    }
+	private boolean IsGray(final Pixel pixel)
+	{
+		double min = Math.min(Math.min(pixel.r, pixel.g), pixel.b);
+		double max = Math.max(Math.max(pixel.r, pixel.g), pixel.b);
+
+		return (max - min) < 40;
+	}
+
+	private double[] FindBounds(Mat image)
+	{
+		double[] bounds = new double[4];
+		bounds[0] = image.width();
+		bounds[2] = image.height();
+
+		Pixel pixel = new Pixel((_lastDetection.color == Detection.Color.Red ? 255 : 0), 0, (_lastDetection.color == Detection.Color.Blue ? 255 : 0), 255);
+
+		for (int i = 0; i < image.height(); i++)
+		{
+			for (int j = 0; j < image.width(); j++)
+			{
+				if (!Arrays.equals(image.get(i, j), pixel.GetData()))
+					continue;
+
+				boolean alone = true;
+				for (int ii = -1; ii <= 1 && alone; ii++)
+					for (int jj = -1; jj <= 1 && alone; jj++)
+						if (!Arrays.equals(image.get(i, j), image.get((jj == 0 ? ii : 0), (ii == 0 ? jj : 0))))
+							alone = false;
+				if (alone)
+					continue;
+
+				if (j < bounds[0])
+					bounds[0] = j;
+				else if (j > bounds[1])
+					bounds[1] = j;
+
+				if (i < bounds[2])
+					bounds[2] = i;
+				else if (i > bounds[3])
+					bounds[3] = i;
+			}
+		}
+
+		return bounds;
+	}
+
+	private void CullPixels(Mat image, int i, int j, int noPixels, double... data)
+	{
+		if (!_debugMode)
+			return;
+
+		for (int ii = i; ii < i + noPixels; ii++)
+			for (int jj = j; jj < j + noPixels; jj++)
+				image.put(ii, jj, data);
+	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Misc/IndustrialDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Misc/IndustrialDemo.java
@@ -4,66 +4,85 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.autonom.OpenCV.AprilTagDetectionPipeline;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.concurrent.TimeUnit;
 
 
 @Autonomous(group = "autonom")
 public class IndustrialDemo extends LinearOpMode
 {
-    SampleMecanumDrive mecanumDrive;
-    OpenCvCamera camera;
-    CustomPipeline pipeline;
-    int cameraMonitorViewId = 0;
+	OpenCvWebcam webcam;
+	CustomPipeline pipeline;
+	int cameraMonitorViewId = 0;
 
-    @Override
-    public void runOpMode() throws InterruptedException
-    {
-        Init();
+	@Override
+	public void runOpMode() throws InterruptedException
+	{
+		Init();
 
-        waitForStart();
+		waitForStart();
 
-        while (!isStopRequested())
-            Run();
-    }
+		pipeline.SetDebug(false);
 
-    private void Init()
-    {
-        cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        pipeline = new CustomPipeline();
+		while (!isStopRequested())
+			Run();
+	}
 
-        camera.setPipeline(pipeline);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
-            @Override
-            public void onOpened()
-            {
-                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
-            }
+	private void Init()
+	{
+		telemetry.setMsTransmissionInterval(100);
 
-            @Override
-            public void onError(int errorCode)
-            {
+		pipeline = new CustomPipeline();
 
-            }
-        });
+		cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+		webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+		webcam.setMillisecondsPermissionTimeout(2500);
+		webcam.setPipeline(pipeline);
+		webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+		{
+			@Override
+			public void onOpened()
+			{
+				webcam.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+				ExposureControl exposure = webcam.getExposureControl();
+				exposure.setMode(ExposureControl.Mode.Auto);
 
+				GainControl gain = webcam.getGainControl();
+				gain.setGain(30);
+			}
 
-        telemetry.setMsTransmissionInterval(100);
-    }
+			@Override
+			public void onError(int errorCode)
+			{
+				telemetry.addData("Cam Error: ", errorCode);
+				telemetry.update();
+			}
+		});
+	}
 
-    private void Run()
-    {
-        Telemetry();
-    }
+	private void Run()
+	{
+		Telemetry();
+	}
 
-    private void Telemetry()
-    {
-        telemetry.addLine(pipeline.GetLastDetection().name());
-        telemetry.update();
-    }
+	private void Telemetry()
+	{
+		telemetry.addData("Frame Count", webcam.getFrameCount());
+		telemetry.addData("FPS", webcam.getFps());
+		telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+		telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+		telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+		telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+		telemetry.addData("Exposure: ", webcam.getExposureControl().getExposure(TimeUnit.MILLISECONDS));
+		telemetry.addData("Gain: ", webcam.getGainControl().getGain());
+		telemetry.addLine();
+		telemetry.addData("Object: ", pipeline.GetLastDetection().color.name() + " " + pipeline.GetLastDetection().shape.name());
+		telemetry.update();
+	}
 }
