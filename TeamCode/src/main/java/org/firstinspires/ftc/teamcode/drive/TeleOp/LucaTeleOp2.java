68cbca6396927b290b83cd6ffac3611fde18219b
Vlad_ <vladup07@gmail.com>
2023-10-15 20:01:57 +0300

    ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOp/LucaTeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOp/LucaTeleOp2.java
@@ -5,17 +5,11 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.PIDCoefficients;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @TeleOp(name = "LucaTeleOp2", group = "Driving")
@@ -27,8 +21,13 @@ public class LucaTeleOp2 extends LinearOpMode
 
 	DcMotorEx intakeMotor;
 	DcMotorEx liftMotor1, liftMotor2;
+	DcMotorEx tumblerMotor;
 
-	Servo clawServo, tumblerServo1, tumblerServo2, backflipperServo;
+	int IdleTumblerPos = 200;
+	int BoardTumblerPos = 450;
+	int LoadTumblerPos = 0;
+
+	Servo clawServo, lockerServo, rotatorServo;
 
 	@Override
 	public void runOpMode() throws InterruptedException
@@ -48,14 +47,27 @@ public class LucaTeleOp2 extends LinearOpMode
 
 		intakeMotor = hardwareMap.get(DcMotorEx.class, "slot2");
 		intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
 		liftMotor1 = hardwareMap.get(DcMotorEx.class, "slot3");
 		liftMotor2 = hardwareMap.get(DcMotorEx.class, "slot7");
-		liftMotor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-		liftMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		liftMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		liftMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		liftMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		liftMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		liftMotor2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+		tumblerMotor = hardwareMap.get(DcMotorEx.class, "slot");
+		tumblerMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		tumblerMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		tumblerMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		tumblerMotor.setTargetPosition(0);
+		tumblerMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
 
 		clawServo = hardwareMap.get(Servo.class, "servo0");
+		lockerServo = hardwareMap.get(Servo.class, "servo1");
+		rotatorServo = hardwareMap.get(Servo.class, "servo2");
 
 		wheelGamepad = gamepad1;
 		armGamepad = gamepad2;
@@ -70,34 +82,57 @@ public class LucaTeleOp2 extends LinearOpMode
 		Lift();
 		Claw();
 		Tumbler();
+		Rotator();
+		Locker();
 		Telemetry();
 	}
 
-	private boolean safe1 = false;
-	private double lastTumblerPos = 0.0;
-	private double lastBackflipperPos = 0.0;
 	private void Tumbler()
 	{
-		if (armGamepad.b && !safe1)
+		if(armGamepad.dpad_up)
+			tumblerMotor.setTargetPosition(BoardTumblerPos);
+		else if(armGamepad.dpad_down)
+			tumblerMotor.setTargetPosition(LoadTumblerPos);
+		else if(armGamepad.dpad_left)
+			tumblerMotor.setTargetPosition(IdleTumblerPos);
+		tumblerMotor.setPower(0.5);
+		tumblerMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+	}
+
+	private boolean safe0 = false;
+	private double lastServoPos0 = 0.0;
+	private void Claw()
+	{
+		if (armGamepad.a && !safe0)
 		{
-			tumblerServo1.setPosition(lastTumblerPos == 0.0 ? (lastTumblerPos = 0.7) : (lastTumblerPos = 0.0));
-			tumblerServo2.setPosition(lastTumblerPos == 0.0 ? (lastTumblerPos = 0.7) : (lastTumblerPos = 0.0));
-			backflipperServo.setPosition(lastBackflipperPos == 0.0 ? (lastBackflipperPos = 1.0) : (lastBackflipperPos = 0.0));
+			clawServo.setPosition(lastServoPos0 == 0.0 ? (lastServoPos0 = 0.5) : (lastServoPos0 = 0.0));
+			safe0 = true;
+		}
+		else safe0 = armGamepad.a;
+	}
+
+	private boolean safe1 = false;
+	private double lastServoPos1 = 0.0;
+	private void Rotator()
+	{
+		if(armGamepad.b && !safe1 && tumblerMotor.getCurrentPosition() > IdleTumblerPos)
+		{
+			rotatorServo.setPosition(lastServoPos1 == 0.0 ? (lastServoPos1 = 1.0) : (lastServoPos1 = 0.0));
 			safe1 = true;
 		}
 		else safe1 = armGamepad.b;
 	}
 
-	private boolean safe0 = false;
-	private double lastServoPos = 0.0;
-	private void Claw()
+	private boolean safe2 = false;
+	private double lastServoPos2 = 0.0;
+	private void Locker()
 	{
-		if (armGamepad.a && !safe0)
+		if(armGamepad.y && !safe2)
 		{
-			clawServo.setPosition(lastServoPos == 0.0 ? (lastServoPos = 1.0) : (lastServoPos = 0.0));
-			safe0 = true;
+			lockerServo.setPosition(lastServoPos2 == 0.0 ? (lastServoPos2 = 1.0) : (lastServoPos2 = 0.2));
+			safe2 = true;
 		}
-		else safe0 = armGamepad.a;
+		else safe2 = armGamepad.y;
 	}
 
 	private void Lift()
@@ -110,8 +145,11 @@ public class LucaTeleOp2 extends LinearOpMode
 
 	private void Intake()
 	{
-		if (wheelGamepad.left_bumper)
-			intakeMotor.setPower(1.0);
+//		if (wheelGamepad.left_bumper)
+//			intakeMotor.setPower(1.0);
+//		else
+//			intakeMotor.setPower(0.0);
+		intakeMotor.setPower(wheelGamepad.right_stick_y);
 	}
 
 	private void Wheels()
@@ -137,10 +175,17 @@ public class LucaTeleOp2 extends LinearOpMode
 
 	private void Telemetry()
 	{
-		telemetry.addData("Input: ", gamepad1.left_stick_x + " " + wheelGamepad.left_stick_y);
 		telemetry.addData("Intake Power: ", intakeMotor.getPower());
+
 		telemetry.addData("Lift1 Power: ", liftMotor1.getPower());
 		telemetry.addData("Lift2 Power: ", liftMotor2.getPower());
+		telemetry.addData("Lift2 Pos: ", liftMotor2.getCurrentPosition());
+		telemetry.addData("Lift2 Pos: ", liftMotor2.getCurrentPosition());
+
+		telemetry.addData("Tumbler Power: ", tumblerMotor.getPower());
+		telemetry.addData("Tumbler Pos: ", tumblerMotor.getCurrentPosition());
+		telemetry.addData("Tumbler Target Pos: ", tumblerMotor.getTargetPosition());
+
 		telemetry.update();
 	}
 }
